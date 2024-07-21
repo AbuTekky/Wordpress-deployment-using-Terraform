f49fb4bc4a3bd036c0d0487dd7ebd3f358f18b0d
Amir <121247068+AbuTekky@users.noreply.github.com>
2024-07-21 13:34:55 +0100

    Initial commit with all project files

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Coderco-dev pem"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "Subnet CIDR block"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances"
+  type        = number
+  default     = 1
+}
